4cb6d49066b5884c3397bb143bf820319a1fc919
aysesenses <28685278+aysesenses@users.noreply.github.com>
2021-09-02 01:38:43 +0300

    dataBinding MainActivity.kt

--- app/src/main/java/com/example/kotlinfirebaseauth/MainActivity.kt
+++ app/src/main/java/com/example/kotlinfirebaseauth/MainActivity.kt
@@ -4,19 +4,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import androidx.databinding.DataBindingUtil
+import com.example.kotlinfirebaseauth.databinding.ActivityDashboardBinding
+import com.example.kotlinfirebaseauth.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mAuth: FirebaseAuth
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         mAuth = FirebaseAuth.getInstance()
         val user = mAuth.currentUser
-
 
         Handler().postDelayed({
             if (user != null) {
