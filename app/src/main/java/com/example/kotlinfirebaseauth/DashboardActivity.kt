4b68361b56fc43a0c08e845d7f004f558681c89f
aysesenses <28685278+aysesenses@users.noreply.github.com>
2021-09-02 01:32:17 +0300

    dataBinding DashboardActivity.kt

--- app/src/main/java/com/example/kotlinfirebaseauth/DashboardActivity.kt
+++ app/src/main/java/com/example/kotlinfirebaseauth/DashboardActivity.kt
@@ -2,38 +2,34 @@ package com.example.kotlinfirebaseauth
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
+import com.example.kotlinfirebaseauth.databinding.ActivityDashboardBinding
 import com.google.firebase.auth.FirebaseAuth
 
 class DashboardActivity : AppCompatActivity() {
 
     private lateinit var mAuth: FirebaseAuth
 
+    private lateinit var binding: ActivityDashboardBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
-
-        val id_txt: TextView = findViewById(R.id.id_txt)
-        val name_txt: TextView = findViewById(R.id.name_txt)
-        val email_txt: TextView = findViewById(R.id.email_txt)
-        val sign_out_btn: Button = findViewById(R.id.sign_out_btn)
-
-        val profile_image: ImageView = findViewById(R.id.profile_image)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_dashboard)
 
         mAuth = FirebaseAuth.getInstance()
         val currentUser = mAuth.currentUser
 
-        id_txt.text = currentUser?.uid
-        name_txt.text = currentUser?.displayName
-        email_txt.text = currentUser?.email
+        binding.apply {
+            idTxt.text = currentUser?.uid
+            nameTxt.text = currentUser?.displayName
+            emailTxt.text = currentUser?.email
+        }
 
-        Glide.with(this).load(currentUser?.photoUrl).into(profile_image)
+        Glide.with(this).load(currentUser?.photoUrl).into(binding.profileImage)
 
-        sign_out_btn.setOnClickListener {
+        binding.signOutBtn.setOnClickListener {
             mAuth.signOut()
             val intent = Intent(this, SignInActivity::class.java)
             startActivity(intent)
